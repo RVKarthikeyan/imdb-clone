200552250d40e574b5434b6a7b2becbed47b3758
RVKarthikeyan <karthikeyanrv04@gmail.com>
2024-10-13 22:17:56 +0530

    added dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,32 +1,23 @@
-import localFont from "next/font/local";
-import "./globals.css";
-import Header from "@/components/Header";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Header';
+import Providers from './Providers';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "IMDb Clone",
-  description: "This is a Fun Filled Movie Database Website",
+  title: 'IMDb clone',
+  description: 'This is a movie database clone',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header />
-        {children}
-       
+    <html lang='en'>
+      <body className={inter.className}>
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,11 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {    },
   },
   plugins: [],
+  darkMode:'class',
 };
