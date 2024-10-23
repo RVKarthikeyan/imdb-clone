431027f612a6134d29f4d28fa83174694fedab45
RVKarthikeyan <karthikeyanrv04@gmail.com>
2024-10-23 13:06:06 +0530

    changed page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,28 @@
 import Results from '@/components/Results';
-import React from 'react'
-const API_KEY = process.env.API_KEY
+import React from 'react';
 
-export default async function Home({searchParams}) {
-  const genre = searchParams.genre || 'fetchTrending'
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  // Ensure searchParams is an object and handle undefined cases
+  const genre = searchParams?.genre || 'fetchTrending';
+  
   const res = await fetch(
-    `https://api.themoviedb.org/3${ genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`,
-    {next:{ revalidate:10000}
-  }
+    `https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
   );
-  const data=await res.json();
-  if(!res.ok){
-    throw new Error('Failed to fetch data')
+
+  // Check if the response is ok
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
   }
+
+  const data = await res.json();
   const results = data.results;
 
   return (
     <div>
-      <Results results={results}/>
+      <Results results={results} />
     </div>
-  )
+  );
 }
